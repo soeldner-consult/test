0757eed65b325514368e26e2e4e4e108f54e1470
matthiasSC-BA <55184044+matthiasSC-BA@users.noreply.github.com>
2021-04-21 22:39:28 +0200

    Update main.tf

--- test/main.tf
+++ test/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
-  machine_type = "f1-micro"
+  machine_type = "e2-micro"
 
   boot_disk {
     initialize_params {
