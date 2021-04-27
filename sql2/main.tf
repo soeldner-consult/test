b609cfcb68e3892f05756e823ed497dd0b9886ec
matthiasSC-BA <55184044+matthiasSC-BA@users.noreply.github.com>
2021-04-27 14:39:38 +0200

    Create main.tf

---
+++ sql2/main.tf
@@ -0,0 +1,165 @@
+
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ 
+ resource "google_sql_database" "default" {
+  count      = 1
+  name       = "testdb2"
+  project    = "t-vra-gfk-terraform"
+  instance   = "testdb2"
+  charset    = var.db_charset
+  collation  = var.db_collation
+  
+}
+   
+ /*  
+locals {
+  default_user_host        = "%"
+  ip_configuration_enabled = length(keys(var.ip_configuration)) > 0 ? true : false
+  ip_configurations = {
+    enabled  = var.ip_configuration
+    disabled = {}
+  }
+  databases = { for db in var.additional_databases : db.name => db }
+  users     = { for u in var.additional_users : u.name => u }
+  // HA method using REGIONAL availability_type requires binary logs to be enabled
+  binary_log_enabled = var.availability_type == "REGIONAL" ? true : lookup(var.backup_configuration, "binary_log_enabled", null)
+  backups_enabled    = var.availability_type == "REGIONAL" ? true : lookup(var.backup_configuration, "enabled", null)
+}
+resource "random_id" "suffix" {
+  count = var.random_instance_name ? 1 : 0
+  byte_length = 4
+}
+resource "google_sql_database_instance" "default" {
+  provider            = google
+  project             = "t-vra-gfk-terraform"
+  name                = "testdb"
+  database_version    = "MYSQL_5_7"
+  region              = "europe-west3"
+  deletion_protection = var.deletion_protection
+  settings {
+    tier                        = var.tier
+    activation_policy           = var.activation_policy
+    availability_type           = var.availability_type
+    authorized_gae_applications = var.authorized_gae_applications
+    dynamic "backup_configuration" {
+      for_each = [var.backup_configuration]
+      content {
+        binary_log_enabled = local.binary_log_enabled
+        enabled            = local.backups_enabled
+        start_time         = lookup(backup_configuration.value, "start_time", null)
+        location           = lookup(backup_configuration.value, "location", null)
+      }
+    }
+    dynamic "ip_configuration" {
+      for_each = [local.ip_configurations[local.ip_configuration_enabled ? "enabled" : "disabled"]]
+      content {
+        ipv4_enabled    = lookup(ip_configuration.value, "ipv4_enabled", null)
+        private_network = lookup(ip_configuration.value, "private_network", null)
+        require_ssl     = lookup(ip_configuration.value, "require_ssl", null)
+        dynamic "authorized_networks" {
+          for_each = lookup(ip_configuration.value, "authorized_networks", [])
+          content {
+            expiration_time = lookup(authorized_networks.value, "expiration_time", null)
+            name            = lookup(authorized_networks.value, "name", null)
+            value           = lookup(authorized_networks.value, "value", null)
+          }
+        }
+      }
+    }
+    disk_autoresize = var.disk_autoresize
+    disk_size    = var.disk_size
+    disk_type    = var.disk_type
+    pricing_plan = var.pricing_plan
+    user_labels  = var.user_labels
+    dynamic "database_flags" {
+      for_each = var.database_flags
+      content {
+        name  = lookup(database_flags.value, "name", null)
+        value = lookup(database_flags.value, "value", null)
+      }
+    }
+    location_preference {
+      zone = "europe-west3-c"
+    }
+    maintenance_window {
+      day          = var.maintenance_window_day
+      hour         = var.maintenance_window_hour
+      update_track = var.maintenance_window_update_track
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      settings[0].disk_size
+    ]
+  }
+  timeouts {
+    create = var.create_timeout
+    update = var.update_timeout
+    delete = var.delete_timeout
+  }
+  depends_on = [null_resource.module_depends_on]
+}
+resource "google_sql_database" "default" {
+  count      = var.enable_default_db ? 1 : 0
+  name       = var.db_name
+  project    = "t-vra-gfk-terraform"
+  instance   = google_sql_database_instance.default.name
+  charset    = var.db_charset
+  collation  = var.db_collation
+  depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
+}
+resource "google_sql_database" "additional_databases" {
+  for_each   = local.databases
+  project    = "t-vra-gfk-terraform"
+  name       = each.value.name
+  charset    = lookup(each.value, "charset", null)
+  collation  = lookup(each.value, "collation", null)
+  instance   = google_sql_database_instance.default.name
+  depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
+}
+resource "random_id" "user-password" {
+  keepers = {
+    name = google_sql_database_instance.default.name
+  }
+  byte_length = 8
+  depends_on  = [null_resource.module_depends_on, google_sql_database_instance.default]
+}
+resource "google_sql_user" "default" {
+  count      = var.enable_default_user ? 1 : 0
+  name       = var.user_name
+  project    = "t-vra-gfk-terraform"
+  instance   = google_sql_database_instance.default.name
+  host       = var.user_host
+  password   = var.user_password == "" ? random_id.user-password.hex : var.user_password
+  depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
+}
+resource "google_sql_user" "additional_users" {
+  for_each   = local.users
+  project    = "t-vra-gfk-terraform"
+  name       = each.value.name
+  password   = lookup(each.value, "password", random_id.user-password.hex)
+  host       = lookup(each.value, "host", var.user_host)
+  instance   = google_sql_database_instance.default.name
+  depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
+}
+resource "null_resource" "module_depends_on" {
+  triggers = {
+    value = length(var.module_depends_on)
+  }
+}
+*/
